4c8232bb5f02067c314ed907af83cbdfa440f610
nzzb <91313758+nzzb@users.noreply.github.com>
2023-06-29 17:42:00 +0800

    Add files via upload

--- FindFile/src/page/getFilePath.js
+++ FindFile/src/page/getFilePath.js
@@ -1,25 +1,44 @@
 let Path=null;
-
-获取文件夹路径
-$.ajax({
-    url: "src/server/getPath.php",
-    type: "GET",
-    dataType: "json",
-    success: function(response) {
-        console.log("File list:", response);
-
-    },
-    error: function(error) {
-        console.error("Error retrieving file list.", error);
-    }
-  });
+//获取文件夹路径
 
 $(document).ready(function() {
     $("#SelectBtn").click(function() {
-        
-    });
-  });
-$("#folder-input").on("change", function(event) {
+        //获取文件夹路径
+
+        $.ajax({
+            url: "src/server/getPath.php",
+            type: "GET",
+            success: function(data) {
+              console.log(data); 
+            },
+            error: function(error) {
+              console.log(error); 
+            }
+        });
+
+        for(i=0;i<event.target.files.length;i++){
+
+            const folderPath = event.target.files[i].webkitRelativePath; //获取单个文件目录
+            Path = folderPath.split("/");  //获取文件详细地址
+            console.log(folderPath);
+            console.log(Path);
+
+            $.post("src/server/getPath.php",{
+                "folderPath" : folderPath,
+                "Path" : Path,
+                "i" : i
+            },function(data){
+                
+            },"json");
+
+            GetText(folderPath);
+
+        }
+
+    }); 
+
+});
+  $("#ConfirmBtn").on("change", function(event) {
 
     for(i=0;i<event.target.files.length;i++){
 
@@ -41,4 +60,3 @@ $("#folder-input").on("change", function(event) {
     }
 
   }); 
-
--- FindFile/src/server/getPath.php
+++ FindFile/src/server/getPath.php
@@ -1,25 +1,11 @@
 <?php
-    $code=0;
-	$data=[];
-	$msg=["注册失败","注册成功","已经注册"];//提示信息
-    $nowTime=time();
-// 获取本地目录的文件列表
-$file = 'G:/ceshi';
-if (is_readable($directory)) {
-    $code=1;
-    getApiResult($code, $msg[$code],$data);
-} else {
+    session_start();
+    include("function.php");
+
+    $Path="getFilePath.py";
+    $command = "python {$Path}";
+    $data = shell_exec($command);
     
-    getApiResult($code, $msg[$code],$data);
-}
+    echo $data;
 
-// // 获取文件列表
-// $fileList = scandir($file);
-
-// // 过滤掉.和..目录
-// $fileList = array_diff($fileList, array('.', '..'));
-
-// // 返回文件列表到前端
-// header('Content-Type: application/json');
-// echo json_encode($fileList);
 ?>
